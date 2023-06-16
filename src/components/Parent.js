f27bb2139879ccf3a8376a46b1e141c3f2bdb0f0
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-06-16 14:14:36 +0530

    submitting code-Fri Jun 16 2023 14:14:33 GMT+0530 (India Standard Time)

---
+++ src/components/Parent.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Parent = () => {
+  const [modal, setModal] = useState(false);
+
+  const handleButtonClick = () => {
+    setModal(true);
+  };
+
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div className="child">
+        <h2>Child Component</h2>
+        <button onClick={handleButtonClick}>Show Modal</button>
+        {modal && (
+          <div>
+            <h3>Modal Content</h3>
+            <p>This is the modal content.</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Parent;
